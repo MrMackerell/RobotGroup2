5ce6df1998cc47a34d580b0316c7763aa20007dc
MrMackerell <1krishnaktul@hdsb.ca>
2024-05-01 11:37:59 -0400

    Created variables and print their states to the Serial Monitor.  Also set to find out the time lapse between each check

--- infraredReading/infraredReading.ino
+++ infraredReading/infraredReading.ino
@@ -3,6 +3,7 @@
 #define IR_3 10
 #define IR_4 11
 
+unsigned long irSensorMillis = 0; //Timer to track the last report of the IR sensors
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,9 +11,17 @@ void setup() {
   pinMode(IR_2, INPUT);
   pinMode(IR_3, INPUT);
   pinMode(IR_4, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //Get the current run time in milliseconds
+  unsigned long currentMillis = millis();
 
+  //Check the states of the IR sensors every 500ms
+  if (currentMillis - irSensorMillis >= 500) {
+    irSensorMillis = currentMillis;
+    readInfrared();
+  }
 }
